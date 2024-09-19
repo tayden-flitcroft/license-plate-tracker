5199a46548aa504169157573284eeca80dac91ec
Tayden Flitcroft <taydengoodeill@gmail.com>
2024-09-19 17:30:56 -0500

    fixing date

--- src/hooks/useDate.ts
+++ src/hooks/useDate.ts
@@ -9,7 +9,7 @@ interface UseDate {
 
 export const useDate = (): UseDate => {
   const jsDate = new Date()
-  const today = new CalendarDate(jsDate.getFullYear(), jsDate.getMonth(), jsDate.getDate())
+  const today = new CalendarDate(jsDate.getFullYear(), jsDate.getMonth() + 1, jsDate.getDate())
 
   const [date, setDate] = useState<CalendarDate>(today)
 
